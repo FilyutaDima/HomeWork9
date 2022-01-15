e50e9cdf1426537ce6b85d5c8adca99377f80fdc
Dmitry <filyuta.dima@yandex.ru>
2022-01-15 09:01:50 +0300

    Traffic light logic implemented

--- HomeWork9/HomeWork9/ViewController.swift
+++ HomeWork9/HomeWork9/ViewController.swift
@@ -8,11 +8,106 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    enum Properties: Double {
+        case transparent = 0.2
+        case nonTransparent = 1
+    }
+    
+    enum Positions: Int {
+        case first = 1
+        case second = 2
+        case third = 3
+    }
+    
+    enum Modes: String {
+        case start = "Start"
+        case next = "Next"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setTransparent(view: redSirnal, property: .transparent)
+        makeCircle(from: redSirnal)
+        setTransparent(view: yellowSignal, property: .transparent)
+        makeCircle(from: yellowSignal)
+        setTransparent(view: greenSignal, property: .transparent)
+        makeCircle(from: greenSignal)
+        
+        setTitleButton(title: .start)
+        button.layer.cornerRadius = 10
     }
 
+    @IBOutlet var redSirnal: UIView!
+    @IBOutlet var yellowSignal: UIView!
+    @IBOutlet var greenSignal: UIView!
+    @IBOutlet var button: UIButton!
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        if button.title(for: .normal) == Modes.start.rawValue {
+            setTitleButton(title: .next)
+        }
+        
+        switch position {
+        case Positions.first.rawValue:
+            changeDirection()
+            
+            setTransparent(view: redSirnal, property: .nonTransparent)
+            setTransparent(view: yellowSignal, property: .transparent)
+            setTransparent(view: greenSignal, property: .transparent)
+            
+            toNextPosition()
+        case Positions.second.rawValue:
+            if direction {
+                setTransparent(view: redSirnal, property: .nonTransparent)
+                setTransparent(view: yellowSignal, property: .nonTransparent)
+                setTransparent(view: greenSignal, property: .transparent)
+                
+                toNextPosition()
+            } else {
+                setTransparent(view: redSirnal, property: .transparent)
+                setTransparent(view: yellowSignal, property: .nonTransparent)
+                setTransparent(view: greenSignal, property: .transparent)
+                
+                toPrevPosition()
+            }
+        case Positions.third.rawValue:
+            changeDirection()
+            
+            setTransparent(view: redSirnal, property: .transparent)
+            setTransparent(view: yellowSignal, property: .transparent)
+            setTransparent(view: greenSignal, property: .nonTransparent)
+            
+            toPrevPosition()
+        default:
+            break
+        }
+    }
+    
+    private func makeCircle(from view: UIView) {
+        view.layer.cornerRadius = view.frame.width / 2
+    }
+    
+    private func setTransparent(view: UIView, property: Properties) {
+        view.alpha = property.rawValue
+    }
+    
+    private func setTitleButton(title: Modes) {
+        button.setTitle(title.rawValue, for: .normal)
+    }
+    
+    private func changeDirection() {
+        direction.toggle()
+    }
+    
+    private func toPrevPosition() {
+        position -= 1
+    }
+    
+    private func toNextPosition() {
+        position += 1
+    }
+    
+    var direction = false
+    var position = 1
 }
-
