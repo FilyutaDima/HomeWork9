d3daf8f57ee7f9fc67c55991f392cfa3902a5221
Dmitry <filyuta.dima@yandex.ru>
2022-01-15 08:18:16 +0300

    Project created

---
+++ HomeWork9/HomeWork9/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HomeWork9
+//
+//  Created by Dmitry on 15.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
